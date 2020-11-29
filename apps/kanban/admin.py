a97a897358811f99e40640f9bb320ca4d3ae44c2
mellkior <mlmandych@roadrunner.com>
2020-11-29 10:23:12 -0500

    Registered models with admin site

--- apps/kanban/admin.py
+++ apps/kanban/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Board, Member, Container, Card, AttachmentType, Attachment, Tag, Label
 
 # Register your models here.
+admin.site.Register(Board)
+admin.site.Register(Member)
+admin.site.Register(Container)
+admin.site.Register(Card)
+admin.site.Register(AttachmentType)
+admin.site.Register(Attachment)
+admin.site.Register(Tag)
+admin.site.Register(Label)
