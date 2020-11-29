4532880b771b04baf3ab93c04e6ec43a5b97cfb1
mellkior <mlmandych@roadrunner.com>
2020-11-29 10:24:52 -0500

    Ammends last commit

--- apps/kanban/admin.py
+++ apps/kanban/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from .models import Board, Member, Container, Card, AttachmentType, Attachment, Tag, Label
 
-# Register your models here.
-admin.site.Register(Board)
-admin.site.Register(Member)
-admin.site.Register(Container)
-admin.site.Register(Card)
-admin.site.Register(AttachmentType)
-admin.site.Register(Attachment)
-admin.site.Register(Tag)
-admin.site.Register(Label)
+# register your models here.
+admin.site.register(Board)
+admin.site.register(Member)
+admin.site.register(Container)
+admin.site.register(Card)
+admin.site.register(AttachmentType)
+admin.site.register(Attachment)
+admin.site.register(Tag)
+admin.site.register(Label)
