a54e46576db5440e221e84f68e0848aac99c7acc
mellkior <mlmandych@roadrunner.com>
2021-01-24 16:37:41 -0500

    Cleaned up imports

--- apps/kanban/views.py
+++ apps/kanban/views.py
@@ -1,7 +1,7 @@
-from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework.authentication import (SessionAuthentication,
                                            TokenAuthentication)
+from rest_framework.response import Response
 
 from .models import (Attachment, AttachmentType, Board, Card, Container,
                      KanBanUser, Label, Member, Tag)
